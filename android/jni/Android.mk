0f62567b7f1329955a5a97e2d2e096e2e62a4af3
trimpsyw <njustyw@gmail.com>
2015-03-04 14:36:35 +0800

    新的Android界面

--- android/jni/Android.mk
+++ android/jni/Android.mk
@@ -6,8 +6,16 @@ LOCAL_MODULE := asmlibrary
 LOCAL_SRC_FILES := so/$(TARGET_ARCH_ABI)/libasmlibrary.so
 include $(PREBUILT_SHARED_LIBRARY) 
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := opencv_java
+LOCAL_SRC_FILES := so/$(TARGET_ARCH_ABI)/libopencv_java.so
+include $(PREBUILT_SHARED_LIBRARY) 
 
 include $(CLEAR_VARS)
+
+OPENCV_CAMERA_MODULES:=off
+OPENCV_INSTALL_MODULES:=on
+OPENCV_LIB_TYPE:=STATIC
 
 #OPENCV_CAMERA_MODULES:=off
 #OPENCV_INSTALL_MODULES:=off
@@ -19,7 +27,7 @@ include /media/work/Develop/opencv/OpenCV-2.4.6-android-sdk/sdk/native/jni/OpenC
 LOCAL_SRC_FILES  := DemoFit.cpp AAM_IC.cpp AAM_PDM.cpp AAM_Shape.cpp AAM_TDM.cpp AAM_PAW.cpp AAM_Util.cpp
 LOCAL_C_INCLUDES += $(LOCAL_PATH)
 LOCAL_CFLAGS    += -DOPENCV_OLDER_VISION
-LOCAL_LDLIBS     += -llog -ldl  
+LOCAL_LDLIBS     += -llog -ldl
 
 LOCAL_MODULE     := jni-asmlibrary
 
